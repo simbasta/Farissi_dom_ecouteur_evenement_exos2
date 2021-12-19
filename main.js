d586df7cec0c01d3b8f81d269dd7714343d30d29
waieki <walid1551@hotmail.com>
2021-12-19 04:14:23 +0100

    WF exos fini

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// tout les exos, exo1, exo2, exo3
+
+// les variables
+let exo1 = document.querySelector("#content")
+
+// lancement du programe
+exo1.addEventListener("click", function(e){
+   
+    console.log(exo1.target.className = "border");
+
+})
